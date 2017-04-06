0bd426548140822ef733ea3a7e8279862ce26f25
blose <mpui@tut.by>
2017-04-06 17:45:59 +0300

    added cars xml example

---
+++ Cars/Cars/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Cars
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.brandCombo = new System.Windows.Forms.ComboBox();
+            this.modelCombo = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // brandCombo
+            // 
+            this.brandCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.brandCombo.FormattingEnabled = true;
+            this.brandCombo.Location = new System.Drawing.Point(30, 26);
+            this.brandCombo.Name = "brandCombo";
+            this.brandCombo.Size = new System.Drawing.Size(121, 21);
+            this.brandCombo.TabIndex = 0;
+            this.brandCombo.SelectedIndexChanged += new System.EventHandler(this.brandCombo_SelectedIndexChanged);
+            // 
+            // modelCombo
+            // 
+            this.modelCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.modelCombo.FormattingEnabled = true;
+            this.modelCombo.Location = new System.Drawing.Point(191, 27);
+            this.modelCombo.Name = "modelCombo";
+            this.modelCombo.Size = new System.Drawing.Size(121, 21);
+            this.modelCombo.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 270);
+            this.Controls.Add(this.modelCombo);
+            this.Controls.Add(this.brandCombo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox brandCombo;
+        private System.Windows.Forms.ComboBox modelCombo;
+    }
+}
+
---
+++ Cars/Cars/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Cars
+{
+    public partial class Form1 : Form
+    {
+        XmlDocument carsDocument = new XmlDocument();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            carsDocument.Load("../../cars.xml");
+            /*foreach (XmlNode brand in carsDocument["cars"])
+            {
+                string name = brand["name"].InnerText;
+                brandCombo.Items.Add(name);
+            }*/
+            var brandNodes = carsDocument.SelectNodes(
+                "/cars/brand/name");
+            foreach (XmlNode nameNode in brandNodes)
+            {
+                brandCombo.Items.Add(nameNode.InnerText);
+            }
+        }
+
+        private void brandCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string xpath =
+                "/cars/brand[name='" + (string) brandCombo.SelectedItem + "']/models/model/name";
+            this.Text = xpath;
+            var modelNodes = carsDocument.SelectNodes(xpath);
+            modelCombo.Items.Clear();
+            foreach (XmlNode nameNode in modelNodes)
+            {
+                modelCombo.Items.Add(nameNode.InnerText);
+            }
+        }
+    }
+}
