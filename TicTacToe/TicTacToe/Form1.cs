afa9c68e38daa162552c583957d85803f0d28f3f
blose <mpui@tut.by>
2017-04-11 16:15:37 +0300

    new project TicTacToe

---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        private Button[,] fieldButtons;
+        private GameModel model;
+
+        private Dictionary<GameModel.Side, string> symbols = new Dictionary<GameModel.Side, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            model = new GameModel();
+
+            model.UpdateView += model_UpdateView;
+
+            symbols[GameModel.Side.x] = "x";
+            symbols[GameModel.Side.o] = "o";
+            symbols[GameModel.Side.none] = "";
+
+            fieldButtons = new Button[3,3];
+            for (int i = 0; i < fieldButtons.GetLength(0); i++)
+            {
+                for (int j = 0; j < fieldButtons.GetLength(1); j++)
+                {
+                    Button b = new Button();
+                    b.Size = new Size(50,50);
+                    b.Tag = new Point(i,j);
+                    b.Top = j * 55 + 20;
+                    b.Left = i * 55 + 20;
+                    b.Click += b_Click;
+                    this.Controls.Add(b);
+                    fieldButtons[i, j] = b;
+                }
+            }
+
+        }
+
+
+        void model_UpdateView(GameModel model)
+        {
+            for (int i = 0; i < fieldButtons.GetLength(0); i++)
+            {
+                for (int j = 0; j < fieldButtons.GetLength(1); j++)
+                {
+                    fieldButtons[i, j].Text = symbols[model.Field[i, j]];
+                }
+            }
+            toolStripStatusLabel1.Text = "Ходят " + symbols[model.CurrentTurn];
+        }
+
+        void b_Click(object sender, EventArgs e)
+        {
+            Button b = (Button) sender;
+            Point p = (Point) b.Tag;
+            model.MakeMove(p.X,p.Y,model.CurrentTurn);
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/GameModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class GameModel
+    {
+        /*
+         * 2 игроку по очереди делают ходы
+         * используют х или о
+         * первый ходит х
+         * поле поле 9 клеток
+         * побеждает 3 фигуры в ряд
+         * ничья когда все поля заняты и нет победы
+         * нельзя ходить в занятую клетку
+         */
+
+        public enum Side
+        {
+            x,
+            o,
+            none
+        }
+
+        public delegate void UpdateViewDelegate(GameModel model);
+
+        public event UpdateViewDelegate UpdateView;
+
+        public Side CurrentTurn { get; private set; }
+        public Side[,] Field { get; private set; }
+        public bool GameOver { get; private set; }
+        public Side Winner { get; private set; }
+        public int TurnCount{ get; private set; }
+
+        public GameModel()
+        {
+            TurnCount = 0;
+            CurrentTurn = Side.x;
+            Field = new Side[3,3];
+            for (int i = 0; i < Field.GetLength(0); i++)
+            {
+                for (int j = 0; j < Field.GetLength(1); j++)
+                {
+                    Field[i, j] = Side.none;
+                }
+            }
+            if (UpdateView != null)
+                UpdateView(this);
+        }
+
+        public void MakeMove(int i, int j, Side side)
+        {
+            if (side != CurrentTurn)
+            {
+                throw new Exception("Not your move");
+            }
+            if (Field[i, j] != Side.none)
+            {
+                throw new Exception("Cell not empty");
+            }
+            if (i < 0 || i > 2 || j < 0 || j > 2)
+            {
+                throw new Exception("Field index out of range");
+            }
+            if (GameOver)
+            {
+                throw new Exception("Game is over");
+            }
+            Field[i, j] = side;
+            TurnCount++;
+
+            this.CheckGameOver();
+            CurrentTurn = (CurrentTurn == Side.x) ? Side.o : Side.x;
+            if (UpdateView != null)
+                UpdateView(this);
+        }
+
+        private void CheckGameOver()
+        {
+            if (TurnCount < 5)
+            {
+                return;
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
