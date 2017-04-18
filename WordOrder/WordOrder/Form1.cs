5c4448614a2e78623d72196305fc5d55abe2c5f2
blose <mpui@tut.by>
2017-04-18 17:01:13 +0300

    Added word order

---
+++ WordOrder/WordOrder/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordOrder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private  List<Label> labels = new List<Label>();
+
+        private Point MouseCoordinates;
+        private Point LableCoordinates;
+        private Label RememberedLabel;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Model.Task task = Model.MakeTask();
+            int left = 20;
+            foreach (string word in task.ShuffledSentence)
+            {
+                Label l = new Label();
+                l.Text = word;
+                l.AutoSize = true;
+                l.Left = left;
+                l.Font = new Font("Arial", 24, FontStyle.Regular);
+                this.Controls.Add(l);
+                left += l.Width;
+                l.MouseDown += l_MouseDown;
+                l.MouseUp += l_MouseUp;
+                l.MouseMove +=l_MouseMove;
+                labels.Add(l);
+            }
+        }
+
+
+        void l_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (RememberedLabel != null)
+            {
+                RememberedLabel = null;
+
+                labels.Sort(labelByLeftComparer);
+
+                string text = "";
+                foreach (var l in labels)
+                {
+                    text += l.Text+' ';
+                }
+                MessageBox.Show(text);
+            }
+
+        }
+
+        private static int labelByLeftComparer(Label a, Label b)
+        {
+            if (b.Left > a.Left)
+            {
+                return -1;
+            }
+            else if (b.Left < a.Left)
+            {
+                return +1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+
+        void l_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (RememberedLabel != null)
+            {
+                RememberedLabel.Left = RememberedLabel.Left + e.X - MouseCoordinates.X;
+                RememberedLabel.Top = RememberedLabel.Top + e.Y - MouseCoordinates.Y;
+                this.Update();
+            }
+        }
+
+        void l_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (RememberedLabel == null)
+            {
+                 MouseCoordinates = new Point(e.X, e.Y);
+                 RememberedLabel = (Label)sender;
+                 LableCoordinates = RememberedLabel.Location;
+            }
+  
+        }
+    }
+}
---
+++ WordOrder/WordOrder/Model.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordOrder
+{
+
+    static class Model
+    {
+
+        private static List<string> Sentences = new List<string>() { "I am master Joda", "регулируемЗеркала пристегиваемся ставимНейтраль заводимся" };
+
+        public class Task
+        {
+            public string CorrectSentence;
+            public List<string> ShuffledSentence;
+
+            public Task(string CorrectSentence)
+            {
+                ShuffledSentence = CorrectSentence.Split(' ').ToList();
+                ShuffledSentence.Shuffle();
+            }
+        }
+
+        public static Task MakeTask()
+        {
+            return new Task(Sentences[0]);
+        }
+    }
+
+    static class Helper
+    {
+        private static Random rng = new Random();
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
